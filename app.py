781f160d82bfe6130bbc215a95c59f14b115ae8f
dpescado20 <46748369+dpescado20@users.noreply.github.com>
2019-01-19 20:48:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,17 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-import dash_bootstrap_components as dbc
 
-UPLOAD_DIRECTORY = "/Project_Marketo_App/app_uploaded_files"
+UPLOAD_DIRECTORY = "/project/app_uploaded_files"
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
+
 # Normally, Dash creates its own Flask server internally. By creating our own,
 # we can create a route for downloading files directly:
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 server = Flask(__name__)
-app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
+app = dash.Dash(server=server)
 
 
 @server.route("/download/<path:path>")
@@ -29,156 +27,32 @@ def download(path):
     return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
 
 
-# app header
-_navbar = dbc.NavbarSimple(
-    children=[
-    ],
-    brand='Data Prox Master',
-    brand_href='#',
-    sticky='top'
-)
-
-# tab contents
-tab1_content = (
-    dbc.Card(
-        dbc.CardBody(
-            [
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            [
-                                dcc.Upload(
-                                    dbc.Button('Upload DY-SOI Data'),
-                                    id='id-upload1',
-                                    multiple=True)
-                            ], width=3
-                        ),
-                    ]
-                ),
-
-                # line space
-                dbc.Row(
-                    [
-                        html.Br()
-                    ]
-                ),
-
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            [
-                                html.Div(id='id-lbl1')
-                            ]
-                        )
-                    ]
-                ),
-
-                # line space
-                dbc.Row(
-                    [
-                        html.Br()
-                    ]
-                ),
-
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            [
-                                dcc.Upload(
-                                    dbc.Button('Upload DY-SOI New Hierarchy'),
-                                    id='id-upload2')
-                            ], width=3
-                        ),
-                    ]
-                ),
-
-                # line space
-                dbc.Row(
-                    [
-                        html.Br()
-                    ]
-                ),
-
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            [
-                                html.Div(id='id-lbl2')
-                            ]
-                        )
-                    ]
-                ),
-
-                # line space
-                dbc.Row(
-                    [
-                        html.Br()
-                    ]
-                ),
-
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            [
-                                dbc.Button('Start Process', id='id-btn1', n_clicks=0)
-                            ]
-                        )
-                    ]
-                ),
-                # line space
-                dbc.Row(
-                    [
-                        html.Br()
-                    ]
-                ),
-
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            [
-                                html.Div(id='id-lbl3')
-                            ]
-                        )
-                    ]
-                )
-            ]
-        )
-    )
-)
-
-# app body
-_body = dbc.Container(
+app.layout = html.Div(
     [
-        # line space
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        html.Br()
-                    ]
-                )
-            ]
+        html.H1("File Browser"),
+        html.H2("Upload"),
+        dcc.Upload(
+            id="upload-data",
+            children=html.Div(
+                ["Drag and drop or click to select a file to upload."]
+            ),
+            style={
+                "width": "100%",
+                "height": "60px",
+                "lineHeight": "60px",
+                "borderWidth": "1px",
+                "borderStyle": "dashed",
+                "borderRadius": "5px",
+                "textAlign": "center",
+                "margin": "10px",
+            },
+            multiple=True,
         ),
-
-        # tab creation
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        dbc.Tabs(
-                            [
-                                dbc.Tab(tab1_content, label='New DY-SOI'),
-                                # dbc.Tab(tab2_content, label='Duplicate Tracker')
-                            ]
-                        )
-                    ]
-                )
-            ]
-        )
-    ]
+        html.H2("File List"),
+        html.Ul(id="file-list"),
+    ],
+    style={"max-width": "500px"},
 )
-# app layout
-app.layout = html.Div([_navbar, _body])
 
 
 def save_file(name, content):
@@ -198,11 +72,6 @@ def uploaded_files():
     return files
 
 
-def del_uploaded_file():
-    for filename in os.listdir(UPLOAD_DIRECTORY):
-        os.remove(os.path.join(UPLOAD_DIRECTORY, filename))
-
-
 def file_download_link(filename):
     """Create a Plotly Dash 'A' element that downloads a file from the app."""
     location = "/download/{}".format(urlquote(filename))
@@ -210,15 +79,12 @@ def file_download_link(filename):
 
 
 @app.callback(
-    Output(component_id='id-lbl1', component_property='children'),
-    [
-        Input(component_id='id-upload1', component_property='filename'),
-        Input(component_id='id-upload1', component_property='contents')
-    ]
+    Output("file-list", "children"),
+    [Input("upload-data", "filename"), Input("upload-data", "contents")],
 )
 def update_output(uploaded_filenames, uploaded_file_contents):
     """Save uploaded files and regenerate the file list."""
-    del_uploaded_file()
+
     if uploaded_filenames is not None and uploaded_file_contents is not None:
         for name, data in zip(uploaded_filenames, uploaded_file_contents):
             save_file(name, data)
@@ -226,11 +92,8 @@ def update_output(uploaded_filenames, uploaded_file_contents):
     files = uploaded_files()
     if len(files) == 0:
         return [html.Li("No files yet!")]
-    elif len(files) == 1:
-        # return [html.Li(filename)for filename in files]
-        return [html.Li(file_download_link(files[0]))]
     else:
-        return [html.Li('Wrong Number of Files .. Please Check')]
+        return [html.Li(file_download_link(filename)) for filename in files]
 
 
 if __name__ == "__main__":
